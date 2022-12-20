e521d1d03061efd9b2df1bcdeb29f771b2877a3f
Joye1024 <s9972023@gm.pu.edu.tw>
2022-12-20 12:02:35 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
 import { createApp } from 'vue'
+
+// 載入vee=validate 驗證套件
+import { Field, Form, ErrorMessage, defineRule, configure } from 'vee-validate'
+import { required, email, min } from '@vee-validate/rules'
+import { localize, setLocale } from '@vee-validate/i18n'
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
+
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// 註冊 vee=validate 參數
+defineRule('required', required)
+defineRule('email', email)
+defineRule('min', min)
+configure({
+  generateMessage: localize({ zh_TW: zhTW }),
+  validateOnInput: true
+})
+setLocale('zh_TW')
+
+const app = createApp(App).use(router)
+
+// 註冊 vee=validate 參數
+app.component('Form', Form)
+app.component('Field', Field)
+app.component('ErrorMessage', ErrorMessage)
+
+app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Form @submit="onSubmit" v-slot="{ errors }">
+    {{ errors }}
+    <div class="mb-3">
+      <label for="email" class="form-label">Email</label>
+      <Field id="email" name="email" type="email" class="form-control"
+               placeholder="請輸入 Email"
+               rules="email|required"
+               :class="{ 'is-invalid': errors['email'] }"
+               v-model="user.email"
+               ></Field>
+      <Error-Message name="email" class="invalid-feedback"></error-message>
+    </div>
+    <button class="btn btn-primary" type="submit">Submit</button>
+    </Form>
   </div>
 </template>
 
@@ -13,6 +27,19 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      user: {}
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log(this.user)
+    }
+  },
+  created () {
+    console.log(this)
   }
 }
 </script>
